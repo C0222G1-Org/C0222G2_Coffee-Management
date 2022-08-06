661deae02f3cb2f7c4769c387fe926e7693aca09
tranthienminh135 <67372816+tranthienminh135@users.noreply.github.com>
2022-08-06 17:28:30 +0700

    update

---
+++ c0222g2-coffee-store/src/main/java/com/coffee/C0222g2CoffeeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C0222g2CoffeeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C0222g2CoffeeStoreApplication.class, args);
+    }
+
+}
